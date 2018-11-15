e9eafe3bcd4157f907ff1ed54327cf2da27836ca
felipewww <felipewww@outlook.com.br>
2018-11-15 18:54:29 -0200

    making feed layout #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+// import {$} from 'protractor';
+// import { } from 'j'
+import * as $ from 'jquery';
+// import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [
+        './app.component.css',
+    ]
 })
+
 export class AppComponent {
-  title = 'social';
+
+    @ViewChild('menu') menu;
+    @ViewChild('main') main;
+
+    title = 'social';
+    constructor(){
+    }
+
+    ngOnInit(){
+        // let centralizedContainers = document.getElementsByClassName('col-center');
+        // this.setLikeButtons();
+    }
+
+    // likeThis(e): void{
+    //     console.log(e);
+    //     console.log(e.target);
+    // }
+    // setLikeButtons(): void{
+        // let likeContainers = document.get
+    // }
+
+    menuAction($btn): void {
+        // console.log(this);
+        // console.log(a);
+        // console.log(event);
+        // let btn = event.target;
+
+        if ( !this.main.nativeElement.classList.contains('menu-hidden') ) {
+            this.main.nativeElement.classList.remove('menu-showing');
+            this.main.nativeElement.classList.add('menu-hidden');
+            $btn.classList.remove('is-active');
+        } else {
+            $btn.classList.add('is-active');
+            this.main.nativeElement.classList.remove('menu-hidden');
+            this.main.nativeElement.classList.add('menu-showing');
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule.forRoot(),
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatIconModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
